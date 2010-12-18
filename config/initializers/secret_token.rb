fac65b5c47dc9ed0e9a91626cb051e4aea6170e5
Abu <abubacker@thoughtworks.com>
2010-12-18 12:55:25 +0530

    Irfan/Abu: initial app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WallC::Application.config.secret_token = '4ad3d91b5ed0298251e4259bfbb538ca7bf28cee21172c6dc29bd33842a65bb57b28ce6d2a366b2053fa14d239ee507317061e975a4032e2d565488610800500'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WallC::Application.config.session_store :cookie_store, :key => '_wall.c_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# WallC::Application.config.session_store :active_record_store
